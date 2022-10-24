450cfc149c6a1540b7ba2ee74e64eaed298a39c2
Diogo Raimundo <DiogoRaimundo@users.noreply.github.com>
2022-10-24 08:59:25 +0100

    [WIP] Go Tour - Flow Control (https://go.dev/tour/flowcontrol/1) - Added more defer info

--- web-service-gin/02_flow_control/12_defer.go
+++ web-service-gin/02_flow_control/12_defer.go
@@ -2,6 +2,8 @@
 // The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
 // Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
 
+// More information about defers (https://go.dev/blog/defer-panic-and-recover)
+
 package main
 
 import "fmt"
